d56f88065ad09b6e3ae5a9f3bbe3980f9062c835
Mugil99 <mugilya99@gmail.com>
2023-08-20 08:35:20 +0530

    theme-drawer

--- src/components/Common/Header/drawer.js
+++ src/components/Common/Header/drawer.js
@@ -1,13 +1,44 @@
-import {useState} from 'react';
-import { IconButton } from '@mui/material';
+import {useEffect, useState} from 'react';
+import { IconButton, Switch } from '@mui/material';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import { Link, NavLink } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function TemporaryDrawer() {
   const [open, setOpen] = useState(false);
+  const [darkMode,setDarkMode]=useState(
+    localStorage.getItem("theme") == "dark" ? true : false
+    );
+    useEffect(()=>{
+      if (localStorage.getItem('theme') === 'dark'){
+          setDark();
+      }else{
+          setLight();
+      }
+    },[]);
+  const changeMode=()=>{
+    setDarkMode(!darkMode);
+    toast.success("Theme Changed");
+    const mode=localStorage.getItem("theme");
+    if(mode=="dark"){
+        setLight();
+    }
+    else{
+        setDark() ;
+    }
+};
 
+const setDark=()=>{
+  localStorage.setItem("theme","dark");
+  document.documentElement.setAttribute("data-theme","dark");
+}
+
+const setLight=()=>{
+  localStorage.setItem("theme","light");
+  document.documentElement.setAttribute("data-theme","light");
+}
   return (
     <div>
      
@@ -20,6 +51,7 @@ export default function TemporaryDrawer() {
             onClose={()=>setOpen(false)}
           >
            <div className='drawer-div'>
+           
            <NavLink to='/'>
                <p className="link">Home</p>
             </NavLink>
@@ -32,6 +64,13 @@ export default function TemporaryDrawer() {
             <NavLink to='/dashboard'>
                 <p className="link">Dashboard</p>
             </NavLink>
+
+            <Switch
+            checked={darkMode}
+            onClick={() => {
+                changeMode();
+            }}
+            />
            </div>
           </Drawer>
       
